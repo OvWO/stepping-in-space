0845c685a1b3cbadf98de143dac33235f28cb014
ovwo <neneo_03@hotmail.com>
2018-04-26 12:42:10 -0700

    Navbar and Contact section finished

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -56,79 +56,58 @@
     </div>
     <section id="contact">
       <div id="skills">
-        <h2>Skills <i class="fas fa-chart-bar" style="color: #259bfa"></i></h2>
-        <p>HTML <i class="fab fa-html5" style="color: #f77729"></i>, CSS <i class="fab fa-css3-alt" style="color: #259bfa"></i> & JS <i class="fab fa-js" style="color: #fae421"></i></p>
+        <h2>Skills <i class="fas fa-chart-bar blue"></i></h2>
+        <p>HTML <i class="fab fa-html5 orange"></i>, CSS <i class="fab fa-css3-alt blue"></i> & JS <i class="fab fa-js yellow"></i></p>
         <div class="container">
           <div class="skills web">80%</div>
         </div>
-        <p>Android <i class="fab fa-android" style="color: #56ff50"></i></p>
+        <p>Android <i class="fab fa-android light-green"></i></p>
         <div class="container">
           <div class="skills android">40%</div>
         </div>
-        {{-- python, Machine Learning and Arduino --}}
-{{--         <p>iOS <i class="fab fa-apple" style="color: #b34dd4"></i></p>
-        <div class="container">
-          <div class="skills iOS">10%</div>
-        </div> --}}
+        <!-- Skills Big Icons -->
         <div id="skills-icons">
-          <h3>Additional Skills</h2>
-                <div class="tooltip">
-                    <span class="tooltiptext">Laravel</span>
-                 <a href="https://laravel.com/"><img src="img/laravel.png" alt="Laravel"></a>
-                 </div>
-                <div class="tooltip">
-                    <span class="tooltiptext">Vuejs</span>
-                 <a href="https://vuejs.org/"><img src="img/vuejs.png" alt="Vuejs"></a>
-                      </div>
-                <div class="tooltip">
-                    <span class="tooltiptext">Bootstrap</span>
-                 <a href="https://getbootstrap.com/"><img src="img/bootstrap.png" alt="Bootstrap"></a>
-             </div>
-                 <div class="tooltip">
-                    <span class="tooltiptext">Materilize</span>
-                 <a href="http://materializecss.com/"><img src="img/materialize.png" alt="Materilize"></a>
-             </div>
-                 {{-- <a href=""><img src="img/PS.png" alt="Adobe Photoshop"> --}}
-                     <div class="tooltip">
-                    <span class="tooltiptext">Adobe Illustrator</span>
-                 <a href="https://es.wikipedia.org/wiki/Adobe_Illustrator"><img src="img/AI.png" alt="Adobe Illustrator"  width="" height=""></a>
-             </div>
-                <p>
-                    {{-- <i class="fas fa-language"></i> --}}
-                {{-- <i class="fas fa-lock"></i> --}}
-                {{-- <i class="fas fa-shopping-bag"></i> --}}
-            </p>
-    </div>
-
-  </div>
-  <div id="vl"></div>
-  <div id="contact-me">
-    <h2>Contact <i class="fas fa-id-card" style="color: #f77729"></i></h2>
-    <form action="">
-      <p>
-        <label for="email"><i class="fas fa-envelope" style="color: #56ff50"></i> Email:</label>
-        <input type="email" name="email" placeholder="Your Email">
-      </p>
-      <p>
-        <label for="message"><i class="fas fa-comment" style="color: #b34dd4"></i> Message:</label>
-        <textarea name="message" cols="30" rows="8" placeholder="Send me a message..."></textarea>
-      </p>
-
-        <button type="submit"><i class="fas fa-paper-plane"></i> Send</button>
-{{--             &nbsp; <a href=""><i class="fab fa-facebook-square"></i></a>
-        <a href=""><i class="fab fa-github"></i></a> --}}
-    </form>
-  </div>
-</section>
-
-<div class="parallax-image1">
-  <div class="parallax-text">
-    <span class="border">
+          <h3>Additional Skills</h3>
+          <div class="tooltip">
+            <span class="tooltiptext">Laravel</span>
+            <a href="https://laravel.com/"><img src="img/laravel.png" alt="Laravel"></a>
+          </div>
+          <div class="tooltip">
+            <span class="tooltiptext">Vuejs</span>
+            <a href="https://vuejs.org/"><img src="img/vuejs.png" alt="Vuejs"></a>
+          </div>
+          <div class="tooltip">
+            <span class="tooltiptext">Bootstrap</span>
+            <a href="https://getbootstrap.com/"><img src="img/bootstrap.png" alt="Bootstrap"></a>
+          </div>
+          <div class="tooltip">
+            <span class="tooltiptext">Materilize</span>
+            <a href="http://materializecss.com/"><img src="img/materialize.png" alt="Materilize"></a>
+          </div>
+          <div class="tooltip">
+            <span class="tooltiptext">Adobe Illustrator</span>
+            <a href="https://es.wikipedia.org/wiki/Adobe_Illustrator"><img src="img/AI.png" alt="Adobe Illustrator"  width="" height=""></a>
+          </div>
+        </div>
+      </div>
+      <div id="contact-me">
+        <h2>Contact <i class="fas fa-id-card orange"></i></h2>
+        <form action="">
+            <label for="email"><i class="fas fa-envelope light-green"></i> Email:</label>
+            <input type="email" name="email" placeholder="Your Email">
+            <label for="message"><i class="fas fa-comment purple"></i> Message:</label>
+            <textarea name="message" cols="30" rows="8" placeholder="Send me a message..."></textarea>
+            <button type="submit"><i class="fas fa-paper-plane"></i> Send</button>
+        </form>
+      </div>
+    </section>
+    <div class="parallax-image1">
+      <div class="parallax-text">
+        <span class="border">
         THANKS FOR STOPPING BY
       </span>
-  </div>
-</div>
-
+      </div>
+    </div>
   </div>
 </div>
 @endsection
--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,32 +1,33 @@
-{{-- <nav>
-      <div class="box header">Header</div>
+{{--
+<nav>
+  <div class="box header">Header</div>
 </nav>
- --}}
-<div class="background-image"></div>
+--}} {{--
+<div class="background-image"></div> --}}
 <div class="wrapper header">
-    <header>
-      <nav>
-        <div class="menu-icon">
-          <i class="fa fa-bars fa-2x"></i>
-        </div>
-        <div class="logo">
-          WaterWork
-        </div>
-        <div class="menu">
-          <ul>
-            <li><a href="">Home</a></li>
-            <li><a href="">About</a></li>
-            <li><a href="">Blog</a></li>
-            <li><a href="">Contact</a></li>
-          </ul>
-        </div>
-      </nav>
-    </header>
-      <div class="header-content">
-    <h1> Web and Mobile Development </h1>
-    <a href="#about" class="navigate">
-      <div><i class="fas fa-angle-double-down"></i></div>
-    </a>
-  </div>
+  <header>
+    <nav>
+      <div class="menu-icon">
+        <i class="fa fa-bars fa-2x"></i>
+      </div>
+      <div class="logo">
+        WaterWork
+      </div>
+      <div class="menu">
+        <ul>
+          <li><a href="">Home</a></li>
+          <li><a href="">About</a></li>
+          <li><a href="">Blog</a></li>
+          <li><a href="">Contact</a></li>
+        </ul>
+      </div>
+    </nav>
+    <div class="header-content">
+      <h1> Web and Android Development </h1>
+      <br>
+      <a href="#about" class="navigate">
+        <div><i class="fas fa-angle-double-down"></i></div>
+      </a>
     </div>
-
+  </header>
+</div>
