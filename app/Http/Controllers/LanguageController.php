739cb659d23b14d1da1113b8654e7ed4dc3dc7c9
ovwo <neneo_03@hotmail.com>
2018-06-25 23:06:39 -0700

    Try bug fix language

--- app/Http/Controllers/LanguageController.php
+++ app/Http/Controllers/LanguageController.php
@@ -13,9 +13,9 @@ class LanguageController extends Controller
 {
     public function switchLang($lang)
     {
-        if (array_key_exists($lang, Config::get('languages'))) {
+        // if (array_key_exists($lang, Config::get('languages'))) {
             Session::put('applocale', $lang);
-        }
+        // }
         return Redirect::back();
     }
 }
