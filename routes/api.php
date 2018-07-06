f6151985aaed171d8a2eccce5faed7cbcaca6c62
ovwo <neneo_03@hotmail.com>
2018-07-06 00:20:18 -0700

    basic api route added

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,11 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('sample-restful-apis', function()
+{
+    return array(
+      1 => "expertphp",
+      2 => "demo"
+    );
+});
