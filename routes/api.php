58e70c63db4e10ecb3b7b9b53adb84531a16571a
ovwo <neneo_03@hotmail.com>
2018-07-10 16:19:19 -0700

    messy routes api added

--- routes/api.php
+++ routes/api.php
@@ -14,9 +14,29 @@ use Illuminate\Http\Request;
 */
 
 Route::post('register', '\App\Http\Controllers\Api\Auth\RegisterController@register');
-Route::post('login', '\App\Http\Controllers\Api\Auth\LoginController@login');
-Route::post('refresh', '\App\Http\Controllers\Api\Auth\LoginController@refresh');
+// Route::post('login', '\App\Http\Controllers\Api\Auth\LoginController@login');
+// Route::post('refresh', '\App\Http\Controllers\Api\Auth\LoginController@refresh');
 // Route::post('social_auth', '\App\Http\Controllers\Api\Auth\SocialAuthController@socialAuth');
+
+
+Route::middleware('auth:api')->group(function () {
+    Route::post('logout', 'Api\Auth\LoginController@logout');
+
+    Route::get('posts', 'Api\PostController@index');
+
+/*
+    // Show article
+    Route::get('article/{id}', 'Api\ArticleController@show');
+    // Create new article
+    Route::post('article', 'Api\ArticleController@store');
+    // Create new article
+    Route::put('article', 'Api\ArticleController@store');
+    // Delete article
+    Route::delete('article/{id}', 'Api\ArticleController@destroy');*/
+});
+
+
+
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
